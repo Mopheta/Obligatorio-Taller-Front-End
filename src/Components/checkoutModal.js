e40b54b57491766d0eada045cce486ba04b9843e
Ganro <gena.loprete@gmail.com>
2020-03-01 20:14:21 -0300

    Juan sartotri vino a casa y arreglo el modal que teniamos, ya no tiene un color tan deprimente, F por bruno que no confirma la comic con

--- src/Components/checkoutModal.js
+++ src/Components/checkoutModal.js
@@ -7,33 +7,43 @@ import '../myStyles/checkoutModal.scss'
 
 const CheckoutModal = (props) => {
 
-    const { totalAmount } = props.cart;    
+    const { totalAmount } = props.cart;
     let [modalIsOpen, setModalIsOpen] = useState(false);
 
     return (
         <div className="checkout_btn">
-                <button className='btn btn-warning checkout_btn;' onClick={() => setModalIsOpen(true) }>Checkout</button>
-                <Modal
-                className="modalStyle" 
-                isOpen={ modalIsOpen } 
+            <button className='btn btn-warning checkout_btn;' onClick={() => setModalIsOpen(true)}>Checkout</button>
+            <Modal
+                className="modalStyle"
+                isOpen={modalIsOpen}
                 onRequestClose={() => setModalIsOpen(false)}
                 ariaHideApp={false}
-                >
-                <h2>Thanks for your purchase!</h2>
-                <h2>Grand total: ${ totalAmount }</h2>
-                <div>
-                    <button className="btn btn-info" onClick={() => {
-                        props.removeAll();
-                        setModalIsOpen(false)
-                    }} >Close</button>
+                shouldCloseOnOverlayClick={false}
+            >
+                <div className="row">
+                    <div className="col">
+                        <h2>Thanks for your purchase!</h2>
+                    </div>
                 </div>
-                </Modal>
-            
+                <div className="row my-5">
+                    <div className="col">
+                        <h4>Grand total: ${totalAmount}</h4>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col">
+                        <button className="btn btn-info float-right" onClick={() => {
+                            props.removeAll();
+                            setModalIsOpen(false)
+                        }} >Close</button>
+
+                    </div>
+                </div>
+
+            </Modal>
+
         </div>
-    ) 
+    )
 }
 
-
-
 export default CheckoutModal;
-
